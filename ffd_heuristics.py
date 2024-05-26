38cc619cf7f9a02731829f2206a9c68c78cc2f02
Thomas Nguyen <thomas.nguyen@tuhh.de>
2024-05-26 20:58:53 +0200

    Added vector product measure. Sorting can now be done with different measures.

--- ffd_heuristics.py
+++ ffd_heuristics.py
@@ -4,9 +4,9 @@ from functions import *
 
 
 
-def ffd_item_centric(bin_list, item_list):
+def ffd_item_centric(bin_list, item_list, measure=resource_sum):
     print_item_list(item_list)
-    item_list = sorted(item_list, key=resource_sum, reverse=True)
+    item_list = sorted(item_list, key=measure, reverse=True)
     print_item_list(item_list)
 
     for item in item_list:
@@ -19,11 +19,11 @@ def ffd_item_centric(bin_list, item_list):
         print(f"Packing failed. {len(item_list)} Items still remain")
 
 
-def ffd_bin_centric(bin_list, item_list):
+def ffd_bin_centric(bin_list, item_list, measure=resource_sum):
     "Bin centric first fit decrease algorithm"
 
     for bin in bin_list:
-        item_list = sorted(item_list, key=resource_sum, reverse=True)
+        item_list = sorted(item_list, key=measure, reverse=True)
 
         while item_list:
             if min(np.subtract(bin.remaining_cap, item_list[0].resources)) < 0:
@@ -44,8 +44,8 @@ item_list = generate_item_list(30)
 
 
 
-# ffd_bin_centric(bin_list, item_list)
-ffd_item_centric(bin_list, item_list)
+ffd_bin_centric(bin_list, item_list, measure=resource_prod)
+# ffd_item_centric(bin_list, item_list, measure=resource_prod)
 
 print_bin_list(bin_list)
 
--- functions.py
+++ functions.py
@@ -2,3 +2,6 @@ import numpy as np
 
 def resource_sum(item):
     return np.sum(item.resources)
+
+def resource_prod(item):
+    return np.prod(item.resources)
