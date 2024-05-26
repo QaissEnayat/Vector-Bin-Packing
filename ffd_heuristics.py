9ed1ed4d13d594151f42be45613fe5900f76cb1f
Thomas Nguyen <thomas.nguyen@tuhh.de>
2024-05-26 16:15:07 +0200

    First implementation of the First Fit Decreasing Algorithm for VBP.

---
+++ ffd_heuristics.py
@@ -0,0 +1,51 @@
+from bins import Item, Bin
+from generator import *
+from functions import *
+
+
+
+def ffd_item_centric(bin_list, item_list):
+    print_item_list(item_list)
+    item_list = sorted(item_list, key=resource_sum, reverse=True)
+    print_item_list(item_list)
+
+    for item in item_list:
+        for bin in bin_list:
+            if not min(np.subtract(bin.remaining_cap, item_list[0].resources)) < 0:
+                bin.insert_item(item)
+                break
+
+    if item_list:
+        print(f"Packing failed. {len(item_list)} Items still remain")
+
+
+def ffd_bin_centric(bin_list, item_list):
+    "Bin centric first fit decrease algorithm"
+
+    for bin in bin_list:
+        item_list = sorted(item_list, key=resource_sum, reverse=True)
+
+        while item_list:
+            if min(np.subtract(bin.remaining_cap, item_list[0].resources)) < 0:
+                     break
+                     
+            bin.insert_item(item_list[0])
+            item_list.pop(0)
+            print_item_list(item_list)
+    
+    if item_list:
+         print(f"Packing failed. {len(item_list)} Items still remain")
+
+
+
+
+bin_list = generate_bin_list(20)
+item_list = generate_item_list(30)
+
+
+
+# ffd_bin_centric(bin_list, item_list)
+ffd_item_centric(bin_list, item_list)
+
+print_bin_list(bin_list)
+
