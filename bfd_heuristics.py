4248d347a1cb1306f192d7bd3a6e3516002bb464
QaissEnayat <qaiss.enayat@gmail.com>
2024-05-27 14:10:34 +0200

    dev bfd item centric algo

--- bfd_heuristics.py
+++ bfd_heuristics.py
@@ -8,16 +8,20 @@ def bfd_item_centric(items, bins):
     items = sorted(items, key=resource_sum, reverse=True)
     
     while len(items) != 0:
-
+        biggest_item = items[0]
         # taking index of vector with the biggest sum of its components:
         # biggest_item_index = [sum(items[i].resources) for i in range(1,len(items))].index(max([sum(items[i].resources) for i in range(1,len(items))]))
-        biggest_item = items[0]
-        least_feasible_bin = [bin.remaining_cap for bin in bins].min()
-        bins.insert_item(biggest_item)
+        for bin in bins:
+            if (bin[len(bins-1)] && np.all(biggest_item <= bin.remaining_cap == False)):
+                return print('Item {biggest_item} can\'t be placed in any other bin anymore. Aborting algorithm!')
+            if  np.all(biggest_item <= bin.remaining_cap == False):
+                continue
+            bin.insert_item(biggest_item)
         #if biggest_item != placed():
         #    return print('Item {biggest_item} cannot be packed into {bins[i]}!') 
         # If finished
         items.pop(0)
+
 
 def bfd_bin_centric(items, bins):
     """Places Items into bins with BFD Bin Centric algorithm."""
