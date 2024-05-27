1e582981b2c65b8c27c3daf7ac41143df666392e
Thomas <thomasnguyen278@gmail.com>
2024-05-27 13:37:18 +0200

    Added bin sorting to the algorithm. Bin and item sorting is now static before execution of the packing.

--- ffd_heuristics.py
+++ ffd_heuristics.py
@@ -4,8 +4,10 @@ from functions import *
 
 
 
-def ffd_item_centric(bin_list, item_list, measure=resource_sum):
+def ffd_item_centric(bin_list, item_list, measure=resource_sum, bin_measure=bin_remaining_cap_sum):
     print_item_list(item_list)
+
+    bin_list = sorted(bin_list, key=bin_measure, reverse=True)
     item_list = sorted(item_list, key=measure, reverse=True)
     print_item_list(item_list)
 
@@ -19,11 +21,13 @@ def ffd_item_centric(bin_list, item_list, measure=resource_sum):
         print(f"Packing failed. {len(item_list)} Items still remain")
 
 
-def ffd_bin_centric(bin_list, item_list, measure=resource_sum):
+def ffd_bin_centric(bin_list, item_list, measure=resource_sum, bin_measure=bin_remaining_cap_sum):
     "Bin centric first fit decrease algorithm"
 
+    bin_list = sorted(bin_list, key=bin_measure, reverse=True)
+     item_list = sorted(item_list, key=measure, reverse=True)
     for bin in bin_list:
-        item_list = sorted(item_list, key=measure, reverse=True)
+       
 
         while item_list:
             if min(np.subtract(bin.remaining_cap, item_list[0].resources)) < 0:
@@ -35,6 +39,28 @@ def ffd_bin_centric(bin_list, item_list, measure=resource_sum):
     
     if item_list:
          print(f"Packing failed. {len(item_list)} Items still remain")
+
+
+def bin_balancing(bin_list, item_list, measure=resource_sum):
+
+    ###TODO Sort Bin list here
+
+    item_list = sorted(item_list, key=measure, reverse=True)
+
+    while item_list:
+        for bin in bin_list:
+            if not min(np.subtract(bin.remaining_cap, item_list[0].resources)) < 0:
+                bin.insert_item(item_list[0])
+                item_list.pop(0)
+
+                ### TODO Update bin list here
+
+                break
+
+            ###TODO Return failure if item could not be packet
+
+
+
 
 
 
