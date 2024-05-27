89f2f06a4ebae5aaad1606327ce0365ae0e3ac20
Thomas <thomasnguyen278@gmail.com>
2024-05-27 13:41:55 +0200

    Added vector product for the remaining_cap.

--- functions.py
+++ functions.py
@@ -1,4 +1,4 @@
-import numpy as nsort_bins_remaining_capp
+import numpy as np
 
 def resource_sum(item):
     return np.sum(item.resources)
@@ -9,3 +9,5 @@ def resource_prod(item):
 def bin_remaining_cap_sum(bin):
     return np.sum(bin.remaining_cap)
     
+def bin_remaining_cap_prod(bin):
+    return np.prod(bin.remaining_cap)
