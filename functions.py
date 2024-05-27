dbd24bffa4356b2fac1b99848f84d82422d14373
QaissEnayat <qaiss.enayat@gmail.com>
2024-05-27 13:20:07 +0200

    adding bin_remaining_cap_sum to calulate the sum of the remaining cap for each bin

--- functions.py
+++ functions.py
@@ -1,7 +1,11 @@
-import numpy as np
+import numpy as nsort_bins_remaining_capp
 
 def resource_sum(item):
     return np.sum(item.resources)
 
 def resource_prod(item):
     return np.prod(item.resources)
+
+def bin_remaining_cap_sum(bin):
+    return np.sum(bin.remaining_cap)
+    
