b4bdb929fff6dbaffe6f915f9e7cef58a5c6f68b
Thomas Nguyen <thomas.nguyen@tuhh.de>
2024-05-28 12:37:38 +0200

    Added fail condition to ffd_item_centric to prematurely end the algorithm. Changed if conditions to check more efficiently.

--- ffd_heuristics.py
+++ ffd_heuristics.py
@@ -11,16 +11,16 @@ def ffd_item_centric(bin_list, item_list, measure=resource_sum, bin_measure=bin_
     item_list = sorted(item_list, key=measure, reverse=True)
     print_item_list(item_list)
 
-    for item in item_list:
+    while item_list:
         for i in range(0, len(bin_list)):
-            if not min(np.subtract(bin_list[i].remaining_cap, item_list[0].resources)) < 0:
-                bin_list[i].insert_item(item)
-                item_list.pop(0)
+            if not np.all(item_list[0].resources <= bin_list[i].remaining_cap) == False:
+                bin_list[i].insert_item(item_list[0])
+                item_list.pop(0)  
                 print_item_list(item_list)
                 break
 
-            elif i == len(bin_list)-1 and min(np.subtract(bin_list[i].remaining_cap, item_list[0].resources)) < 0:
-                return print(f"Packing failed. {len(item_list)} Items still remain")
+            elif i == len(bin_list)-1 and np.all(item_list[0].resources <= bin_list[i].remaining_cap) == False:
+                return print(f"Failed to pack {item_list[0].resources}.")
 
     return bin_list
 
@@ -69,13 +69,13 @@ def bin_balancing(bin_list, item_list, measure=resource_sum, bin_measure=bin_rem
 
 
 
-bin_list = generate_bin_list(10)
-item_list = generate_item_list(30)
+bin_list = generate_bin_list(2)
+item_list = generate_item_list(10)
 
 
 
 # ffd_bin_centric(bin_list, item_list, measure=resource_prod)
-ffd_item_centric(bin_list, item_list, measure=resource_prod)
+ffd_item_centric(bin_list, item_list, measure=resource_sum, bin_measure=bin_remaining_cap_sum)
 
 print_bin_list(bin_list)
 
