cd18919c6398daea0d7d22495fd39d9778175fd2
Thomas Nguyen <thomas.nguyen@tuhh.de>
2024-05-28 21:53:46 +0200

    Fixed bug where bin attrbutes were printed in association with the wrong bins.

--- ffd_heuristics.py
+++ ffd_heuristics.py
@@ -9,7 +9,7 @@ def ffd_item_centric(item_list, bin_list, measure=resource_sum, bin_measure=bin_
     print_item_list(item_list)
  
     # static pre-sorting of lists with given measures. Sorted in decreasing order.
-    bin_list = sorted(bin_list, key=bin_measure, reverse=True)
+    bin_list = sorted(bin_list, key=bin_measure, reverse=False)
     item_list = sorted(item_list, key=measure, reverse=True)
     print_item_list(item_list)
 
@@ -17,10 +17,9 @@ def ffd_item_centric(item_list, bin_list, measure=resource_sum, bin_measure=bin_
         #Iterate through bin list
         for i in range(0, len(bin_list)):
             # Checks if item can be inserted
-            if not np.all(item_list[0].resources <= bin_list[i].remaining_cap) == False:
+            if np.all(item_list[0].resources <= bin_list[i].remaining_cap) == True:
                 bin_list[i].insert_item(item_list[0])
                 item_list.pop(0)  
-
                 # Printing list at every step for testing purposes.
                 print_item_list(item_list)
                 break
@@ -35,13 +34,13 @@ def ffd_bin_centric(item_list, bin_list, measure=resource_sum, bin_measure=bin_r
     "Bin centric first fit decrease algorithm"
 
     # static pre-sorting of lists with given measures. Sorted in decreasing order.
-    bin_list = sorted(bin_list, key=bin_measure, reverse=True)
+    bin_list = sorted(bin_list, key=bin_measure, reverse=False)
     item_list = sorted(item_list, key=measure, reverse=True)
     for bin in bin_list:
        
         while item_list:
             # Checks if item can be inserted
-            if np.all(item_list[0].resources <= bin.remaining_cap) == False:
+            if  np.all(np.less_equal(item_list[0].resources, bin.remaining_cap)) == False:
                 break
                      
             bin.insert_item(item_list[0])
@@ -61,14 +60,14 @@ def bin_balancing(item_list, bin_list, measure=resource_sum, bin_measure=bin_rem
     """First fit decrease algorithm with bin balancing"""
 
     # Static pre-sorting of lists with given measures
-    bin_list = sorted(bin_list, key=bin_measure, reverse=True)
+    bin_list = sorted(bin_list, key=bin_measure, reverse=False)
     item_list = sorted(item_list, key=measure, reverse=True)
 
     while item_list:
         #Iterate through bin list
         for i in range(0, len(bin_list)):
             # Check if item can be inserted
-            if not np.all(item_list[0].resources <= bin_list[i].remaining_cap) == False:
+            if np.all(item_list[0].resources <= bin_list[i].remaining_cap) == True:
                 bin_list[i].insert_item(item_list[0])
                 item_list.pop(0)
 
@@ -77,7 +76,7 @@ def bin_balancing(item_list, bin_list, measure=resource_sum, bin_measure=bin_rem
                 bin_list.pop(i)
 
                 # Printing lists at every step for thes purposes
-                print_bin_list(bin_list)
+                # print_bin_list(bin_list)
                 print_item_list(item_list)
                 break
 
@@ -97,9 +96,9 @@ item_list = generate_item_list(10)
 
 
 
-# ffd_bin_centric(bin_list=bin_list, item_list=item_list, measure=resource_prod, bin_measure=bin_remaining_cap_prod)
-# ffd_item_centric(bin_list=bin_list, item_list=item_list, measure=resource_prod, bin_measure=bin_remaining_cap_prod)
-bin_balancing(bin_list=bin_list, item_list=item_list, measure=resource_prod, bin_measure=bin_remaining_cap_prod)
+# bin_list = ffd_bin_centric(bin_list=bin_list, item_list=item_list, measure=resource_sum, bin_measure=bin_remaining_cap_sum)
+bin_list = ffd_item_centric(bin_list=bin_list, item_list=item_list, measure=resource_sum, bin_measure=bin_remaining_cap_sum)
+# bin_list = bin_balancing(bin_list=bin_list, item_list=item_list, measure=resource_sum, bin_measure=bin_remaining_cap_sum)
 
 print_bin_list(bin_list)
 
